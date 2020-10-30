bc0e354a63df468c49a1e653b09c4900a00ebe26
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-10-30 17:50:36 -0500

    Implemented shared utilities

--- shared.c
+++ shared.c
@@ -1,1 +1,62 @@
+#include <libgen.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+
 #include "shared.h"
+
+static char *programName = NULL;
+
+static key_t shmkey;
+static int shmid;
+static Shared *shmptr = NULL;
+
+void init(int argc, char **argv) {
+	programName = basename(argv[0]);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+void error(char *fmt, ...) {
+	char buf[BUFFER_LENGTH];
+	va_list args;
+	va_start(args, fmt);
+	vsnprintf(buf, BUFFER_LENGTH, fmt, args);
+	va_end(args);
+	fprintf(stderr, "%s: %s\n", programName, buf);
+	cleanup();
+}
+
+void crash(char *msg) {
+	char buf[BUFFER_LENGTH];
+	snprintf(buf, BUFFER_LENGTH, "%s: %s", programName, msg);
+	perror(buf);
+	cleanup();
+	exit(EXIT_FAILURE);
+}
+
+void allocateSharedMemory() {
+	if ((shmkey = ftok(KEY_PATHNAME, KEY_PROJID)) == -1) crash("ftok");
+	if ((shmid = shmget(shmkey, sizeof(Shared), PERMS | IPC_EXCL | IPC_CREAT)) == -1) crash("shmget");
+	else shmptr = (Shared*) shmat(shmid, NULL, 0);
+}
+
+void attachSharedMemory() {
+	if ((shmkey = ftok(KEY_PATHNAME, KEY_PROJID)) == -1) crash("ftok");
+	if ((shmid = shmget(shmkey, sizeof(Shared), PERMS)) == -1) crash("shmget");
+	else shmptr = (Shared*) shmat(shmid, NULL, 0);
+}
+
+void releaseSharedMemory() {
+	if (shmptr != NULL && shmdt(shmptr) == -1) crash("shmdt");
+	if (shmid > 0 && shmctl(shmid, IPC_RMID, NULL) == -1) crash("shmctl");
+}
+
+Shared *getSharedMemory() {
+	return shmptr;
+}
+
+void cleanup() {
+	releaseSharedMemory();
+}
--- shared.h
+++ shared.h
@@ -10,9 +10,54 @@
 #ifndef SHARED_H
 #define SHARED_H
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define BUFFER_LENGTH 1024
+#define KEY_PATHNAME "."
+#define KEY_PROJID 'o'
+#define PERMS (S_IRUSR | S_IWUSR)
+#define PROCESSES_COUNT 18
+#define DEFAULT_QUANTUM_BASE 10000
+
+/* Time data */
 typedef struct {
-	unsigned long sec;
-	unsigned long ns;
+	unsigned long sec; /* seconds */
+	unsigned long ns; /* nanoseconds */
 } Time;
+
+/* Process Control Block (PCB) data */
+typedef struct {
+	unsigned int priority; /* Queue index */
+	unsigned int localPID; /* Simulated PID */
+	pid_t actualPID; /* Actual PID */
+	Time cpu; /* Time spent on the CPU */
+	Time system; /* Time spent in the system */
+	Time blocked; /* Time spent blocked */
+	Time waiting; /* Time spent waiting */
+} PCB;
+
+/* OS data */
+typedef struct {
+	PCB ptable[PROCESSES_COUNT];
+	Time system;
+} OS;
+
+/* Shared-memory data */
+typedef struct {
+	OS os;
+	unsigned int quantum; /* Base quantum for queues */
+} Shared;
+
+void init(int, char**);
+void error(char *fmt, ...);
+void crash(char*);
+
+void allocateSharedMemory();
+void attachSharedMemory();
+void releaseSharedMemory();
+Shared *getSharedMemory();
+
+void cleanup();
 
 #endif
