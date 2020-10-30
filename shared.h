52f320f97a021b3415b0eb4c2bf64ad6bd8c501e
Jared Diehl <diehl@hoare7.cs.umsl.edu>
2020-10-30 16:29:13 -0500

    Created shared module

---
+++ shared.h
@@ -0,0 +1,18 @@
+/*
+ * shared.h 11/02/20
+ * Jared Diehl (jmddnb@umsystem.edu)
+ * ---------------------------------
+ * Provides data structures,
+ * functions, and variables that are
+ * used in multiple modules.
+ */
+
+#ifndef SHARED_H
+#define SHARED_H
+
+typedef struct {
+	unsigned long sec;
+	unsigned long ns;
+} Time;
+
+#endif
